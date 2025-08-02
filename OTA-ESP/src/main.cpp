5bba22e22b9c94ae82e6393ef7a31eaf29835a49
Prathik Narsetty <prathiknarsetty@gmail.com>
2025-08-01 19:11:51 -0500

    figuring out CRC and how to send data

--- OTA-ESP/src/main.cpp
+++ OTA-ESP/src/main.cpp
@@ -13,9 +13,20 @@ void setup() {
   // so far I have setup linkage to my serial console. Let me setup GPIO pins for driving PA18 and NRST
   pinMode(D12, OUTPUT); // I will use D12 to control PA18
   pinMode(D11, OUTPUT); // let me use D13 ro control NRST
-
+  
+// setup UART linkage
+// Here is the information I have gathered from the BSL datasheet
+/*
+MSPM0 flash-based UART is enabled with following configuration by default:
+• Baud rate: 9600bps
+• Data width: 8 bit
+• One stop bit
+• No parity
+*/
+  Serial2.begin(9600, SERIAL_8N1, D0, D1); // TX and RX on ESP32
+  // make sure to aslo connect grounds 
   delay(1000);
-  resetESP();
+  enterBSL();
 }
 
 void loop() {
@@ -23,7 +34,7 @@ void loop() {
 }
 
 // function definitions
-void resetESP() {
+void enterBSL() {
   digitalWrite(D12, LOW);    // Step 0: ensure PA18 LOW
   digitalWrite(D11, LOW);    // Step 1: NRST LOW (reset)
   delay(10);                 // Hold reset for ~10 ms
@@ -35,7 +46,5 @@ void resetESP() {
   delay(10);                 // Step 4: Hold PA18 high for a bit
 
   // Optional: PA18 can be pulled low now or just left high
-
-
-
 }
+
